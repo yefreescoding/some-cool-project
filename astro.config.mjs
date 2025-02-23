968b32b85914287792a577e46671b91c455ffda1
Yefree Valdez <yefree13santos@outlook.com>
2025-02-23 10:19:21 -0400

    remove all files

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-
-import icon from "astro-icon";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [icon()]
-});
